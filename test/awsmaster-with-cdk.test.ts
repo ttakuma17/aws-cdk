0da254fcf1107c6b6c2eda20fcc877393d7d1add
ttakuma17 <ttanimiya@gmail.com>
2024-07-21 22:20:44 +0900

    CDKセットアップ

---
+++ test/awsmaster-with-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsmasterWithCdk from '../lib/awsmaster-with-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/awsmaster-with-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsmasterWithCdk.AwsmasterWithCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
