b95ff6ec47bb64a6ac38cdd1b07aa271cf4be9ba
ttakuma17 <ttanimiya@gmail.com>
2024-08-04 10:56:22 +0900

    一旦リソース削除する

--- lib/awsmaster-with-cdk-stack.ts
+++ lib/awsmaster-with-cdk-stack.ts
@@ -1,16 +1,8 @@
 import * as cdk from 'aws-cdk-lib'
-import * as sqs from 'aws-cdk-lib/aws-sqs'
 import type { Construct } from 'constructs'
 
 export class AwsmasterWithCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props)
-
-    // The code that defines your stack goes here
-
-    // example resource
-    const _queue = new sqs.Queue(this, 'AwsmasterWithCdkQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300),
-    })
-  }
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props)
+    }
 }
